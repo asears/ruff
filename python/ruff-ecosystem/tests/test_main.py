848f732c9fff23811ad2f47ed5d20395012cc334
asears <asears@users.noreply.github.com>
2024-10-14 05:36:42 -0400

    update ruff tests

--- python/ruff-ecosystem/tests/test_main.py
+++ python/ruff-ecosystem/tests/test_main.py
@@ -1,15 +1,8 @@
 """Test main."""
 
-from enum import Enum
-
 import pytest
 
 from ruff_ecosystem.main import OutputFormat
-
-
-class OutputFormat(Enum):
-    markdown = "markdown"
-    json = "json"
 
 
 @pytest.mark.parametrize(
